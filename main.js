60699799332166e4c3ce7e12e7f380c5562a138a
Victor Ferreira <vffvmode@gmail.com>
2024-07-18 19:37:13 -0300

    Added LocalStorage and Reactions

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
-const {Client} = require('whatsapp-web.js');
+const { Client, LocalAuth } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
 
 const client = new Client({
     webVersionCache: {
         type: 'remote',
         remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html',
-    }
+    },
+    authStrategy: new LocalAuth()
 });
 
 client.once('ready', () => {
@@ -19,9 +20,11 @@ client.on('qr', (qr) => {
 client.on('message_create', async (message) => {
     if (message.body === '!sticker' || message.body === '!s') {
         if (!message.hasMedia) {
+            message.react("❌")
             message.reply("ERRO! Você precisa anexar uma imagem ou GIF junto ao comando.")
             return
         }
+        message.react("✅")
         const media = await message.downloadMedia();
         message.reply(media,undefined,{sendMediaAsSticker: true})
     }
