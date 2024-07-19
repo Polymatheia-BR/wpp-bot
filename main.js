e4f6cfcb5b3acc9fba74dd5504d1d64df53ca7f0
Victor Ferreira <69025753+Polymatheia-BR@users.noreply.github.com>
2024-07-19 14:52:23 -0300

    No-GUI Fix

--- main.js
+++ main.js
@@ -2,6 +2,10 @@ const { Client, LocalAuth } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
 
 const client = new Client({
+    puppeteer: { 
+        headless: true,
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+    },
     webVersionCache: {
         type: 'remote',
         remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html',
